af625167cbed57d138acb121b4403e837dfb21fe
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-11-18 20:06:54 -0700

    read to pull

--- twitterclustering/src/TwitterDataGrab.java
+++ twitterclustering/src/TwitterDataGrab.java
@@ -13,7 +13,9 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 /**
- * @author Dustin Hurst
+ * @author Team Dragon: Dustin, Brayden, Steph, Jeffrey
+ * Hello Team! To run, just go to the green circle with an arrow up top, click on the drop down and select "Run Configuration"
+ * There just go to "arguments" and put something there. Use " " for multiple expressions. Have fun!
  *
  */
 public class TwitterDataGrab {
@@ -23,15 +25,14 @@ public class TwitterDataGrab {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        /*
 		if (args.length < 1) {
             System.out.printf("\u001B[31m Usage \u001B[0m: java twitter4j.examples.search.SearchTweets [query]\n"
             		+ "For [query] more than one word use quotes");
             System.exit(-1);
-        }*/
+        }
         Twitter twitter = new TwitterFactory().getInstance();
         try {
-            Query query = new Query("douche nozzle");
+            Query query = new Query(args[0]);
             QueryResult result; 
             do {
                 result = twitter.search(query);
