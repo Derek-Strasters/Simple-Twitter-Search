866efbaf6843414b3e25e387502e617bb7e1dd89
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-11-18 18:52:52 -0700

    initial commit

---
+++ twitterclustering/src/TwitterDataGrab.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package src;
+
+import java.util.List;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+/**
+ * @author Dustin Hurst
+ *
+ */
+public class TwitterDataGrab {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        /*
+		if (args.length < 1) {
+            System.out.printf("\u001B[31m Usage \u001B[0m: java twitter4j.examples.search.SearchTweets [query]\n"
+            		+ "For [query] more than one word use quotes");
+            System.exit(-1);
+        }*/
+        Twitter twitter = new TwitterFactory().getInstance();
+        try {
+            Query query = new Query("douche nozzle");
+            QueryResult result; 
+            do {
+                result = twitter.search(query);
+                List<Status> tweets = result.getTweets();
+                for (Status tweet : tweets) {
+                    System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+                }
+            } while ((query = result.nextQuery()) != null);
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        }
+	}
+
+}
