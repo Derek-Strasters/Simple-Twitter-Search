062e9380586f08c42f41e54a37eabd6ca3ff929a
badstephbot <steph.gilchrist1@gmail.com>
2015-11-25 16:44:17 -0700

    Added TwitterClustering

--- twitterclustering/src/TwitterDataGrab.java
+++ twitterclustering/src/TwitterDataGrab.java
@@ -24,7 +24,7 @@ public class TwitterDataGrab {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/*
 		if (args.length < 1) {
             System.out.printf("\u001B[31m Usage \u001B[0m: java twitter4j.examples.search.SearchTweets [query]\n"
             		+ "For [query] more than one word use quotes");
@@ -46,7 +46,17 @@ public class TwitterDataGrab {
             te.printStackTrace();
             System.out.println("Failed to search tweets: " + te.getMessage());
             System.exit(-1);
-        }
-	}
-
+       }
+*/
+        String[] tweets = {"first tweet", "second tweet", "third tweet", "I am confused that single words are skipped!",
+        		"testing", "let's see what you can do", "testing out results", "starting the test", 
+        		"did it pass the test?", "not sure if this will work", "will it work?", "I sure hope it works",
+        		"Rain is missing?", "", "missing rain?", "rain is not missing as long as it is not alone", 
+        		"the rainforest is pretty", "I'm ready for vacation!", "where would you go on vacation?", "rain",
+        		"this skips single words!", "why are you skipping single words?", "fourth tweet", "fifth tweet",
+        		"does skip sentences words!", "more stuff about rain?", "sixth tweet", "tweet number seven"};
+        TwitterClustering sentenceClustering = new TwitterClustering(tweets);
+        sentenceClustering.JsonFormat();
+		
+	}	
 }
