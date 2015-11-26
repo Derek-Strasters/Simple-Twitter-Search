6d5596844dbc6df7e46ef3ee68e231fe82d4c4e9
badstephbot <steph.gilchrist1@gmail.com>
2015-11-25 17:28:58 -0700

    Moved jar files, added some comments and TODOs

--- twitterclustering/src/TwitterDataGrab.java
+++ twitterclustering/src/TwitterDataGrab.java
@@ -24,13 +24,15 @@ public class TwitterDataGrab {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-/*
+		
+		/* Commented out to test the SentenceClustering
 		if (args.length < 1) {
             System.out.printf("\u001B[31m Usage \u001B[0m: java twitter4j.examples.search.SearchTweets [query]\n"
             		+ "For [query] more than one word use quotes");
             System.exit(-1);
         }
         Twitter twitter = new TwitterFactory().getInstance();
+        //TODO:: figure out rate limit issues, possible with caching of some sort  
         try {
             Query query = new Query(args[0]);
             QueryResult result; 
@@ -47,14 +49,20 @@ public class TwitterDataGrab {
             System.out.println("Failed to search tweets: " + te.getMessage());
             System.exit(-1);
        }
-*/
-        String[] tweets = {"first tweet", "second tweet", "third tweet", "I am confused that single words are skipped!",
+	   */
+		
+		//TODO:: add Brayden's code here. Pass his array of sentences 
+		// tweets array is for testing -- replace it with the array of sentences Brayden collects 
+		String[] tweets = {"first tweet", "second tweet", "third tweet", "I am confused that single words are skipped!",
         		"testing", "let's see what you can do", "testing out results", "starting the test", 
         		"did it pass the test?", "not sure if this will work", "will it work?", "I sure hope it works",
         		"Rain is missing?", "", "missing rain?", "rain is not missing as long as it is not alone", 
         		"the rainforest is pretty", "I'm ready for vacation!", "where would you go on vacation?", "rain",
         		"this skips single words!", "why are you skipping single words?", "fourth tweet", "fifth tweet",
         		"does skip sentences words!", "more stuff about rain?", "sixth tweet", "tweet number seven"};
+		
+		//TODO:: figure out why single words are skipped and if that is a problem
+		//TODO:: Figure out why some sentences are not being clustered correctly
         TwitterClustering sentenceClustering = new TwitterClustering(tweets);
         sentenceClustering.JsonFormat();
 		
